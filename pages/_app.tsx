84f63855407c68cebf5090c6aefa13e741273fdf
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-16 00:55:25 -0300

    Clean the Index, project is read to start development

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+import Head from 'next/head';
 
 const MyApp: React.FC<AppProps> = ({Component, pageProps}) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>CuboGames</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+
+const Home: React.FC = () => {
+  return <h1>Hello, World!!!</h1>
+}
+
+export default Home;
