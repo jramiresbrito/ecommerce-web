95af366156883e7e95a8b7f5e32806a04ca675d8
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-16 03:20:30 -0300

    Add redux config, root and auth reducers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,28 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
 
-const MyApp: React.FC<AppProps> = ({Component, pageProps}) => {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>Cubo Games</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-      </Head>
-      
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <title>Cubo Games</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+          </Head>
+
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   );
-}
-
+};
 
 export default MyApp;
