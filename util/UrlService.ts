69d39e27e0b169685d7e538f11276abf8fdccd4e
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-19 13:45:47 -0300

    Add Url Service

---
+++ util/UrlService.ts
@@ -0,0 +1,15 @@
+interface UrlServiceRequest {
+  page: string | string[];
+  search?: string;
+}
+
+const UrlService = {
+  execute({ page, search }: UrlServiceRequest): string {
+    return (
+      `${search !== '' ? `?search[name]=${search}` : ''}` +
+      `${search !== '' ? '&' : '?'}page=${page}`
+    );
+  },
+};
+
+export default UrlService;
