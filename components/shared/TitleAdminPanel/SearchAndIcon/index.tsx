0c622e4e3ee8a9cf360fee65ea1b9a2f0da0b8c4
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-19 14:18:36 -0300

    ADD Searching

--- components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
+++ components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { InputGroup, FormControl, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import styles from '../../../../styles/AdminTitle.module.css';
 import StyledButton from '../../StyledButton';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import Link from 'next/link';
+import { useDispatch } from 'react-redux';
+import {
+  setSearch as setSearchRedux,
+  clearSearch,
+} from '../../../../store/modules/admin/shared/search/reducer';
+import { useRouter } from 'next/router';
 
 interface SearchAndIcon {
   icon: IconProp;
+  newPath: string;
 }
 
-const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon }) => {
+const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon, newPath }) => {
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    dispatch(clearSearch());
+  }, []);
+
+  const handleSearch = (): void => {
+    router.replace(router.pathname, '?page=1');
+    dispatch(setSearchRedux(search));
+  };
+
   return (
     <Row>
       <Col lg={9} xs>
         <Row>
           <Col lg={9} xs={10}>
             <InputGroup>
-              <FormControl placeholder="Pesquisar" className={styles.input} />
+              <FormControl
+                placeholder="Pesquisar"
+                className={styles.input}
+                value={search}
+                onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+                  setSearch(evt.target.value)
+                }
+                onKeyPress={(evt: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (evt.key.toLocaleLowerCase() === 'enter') {
+                    handleSearch();
+                  }
+                }}
+              />
             </InputGroup>
           </Col>
 
@@ -27,13 +60,18 @@ const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon }) => {
               size="lg"
               color="var(--color-gray-light)"
               className="float-left"
+              onClick={handleSearch}
             />
           </Col>
         </Row>
       </Col>
 
       <Col lg={2} xs={{ span: 3 }} className={styles.titleButton}>
-        <StyledButton icon={icon} type_button="blue" />
+        <Link href={newPath}>
+          <a>
+            <StyledButton icon={icon} type_button="blue" />
+          </a>
+        </Link>
       </Col>
     </Row>
   );
