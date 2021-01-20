049618aec57848a95ec2998bf46f8e37a1473424
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-20 15:00:19 -0300

    ADD Products Service create, update, destroy

--- services/products.ts
+++ services/products.ts
@@ -11,4 +11,21 @@ const ProductsService = {
   index: (url: string) => {
     return api.get<IProductsIndexData>(url).then((response) => response.data);
   },
+
+  create: (product: FormData) => {
+    return api.post<void>('/admin/v1/products', product);
+  },
+
+  update: (product: FormData) => {
+    return api.patch<void>(
+      `/admin/v1/products/${product.get('product[id]')}`,
+      product
+    );
+  },
+
+  delete: (id: number) => {
+    return api.delete<void>(`/admin/v1/products/${id}`);
+  },
 };
+
+export default ProductsService;
