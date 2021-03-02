0c2be12a8d69527e9fe8f1146296bac955afc1fc
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-03-02 10:50:36 -0300

    Improve Responsiveness

--- components/shared/Header/AdminHeader/index.tsx
+++ components/shared/Header/AdminHeader/index.tsx
@@ -25,7 +25,7 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
 
   return (
     <Row className={styles.background}>
-      <Col lg={6} xs={9}>
+      <Col lg={6} xs={12} className={styles.menu}>
         <Link href="/Admin">
           <a>
             <FontAwesomeIcon
@@ -105,14 +105,9 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
         </Link>
       </Col>
 
-      <Col lg={6} xs={3} className="d-none d-md-block">
-        <div className="float-right">
-          <span className={styles.name}>{name}</span>
-          <FontAwesomeIcon
-            icon={faUserCircle}
-            color="var(--color-gray-light)"
-          />
-        </div>
+      <Col lg={6} xs={3} className={styles.profile}>
+        <span className={styles.name}>{name}</span>
+        <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
       </Col>
     </Row>
   );
